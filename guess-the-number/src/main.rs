53d7c76edab1ec3d41e31810dd4a76706d2c9da6
Danial Raza <danialrazafb@gmail.com>
2023-11-30 21:43:08 +0100

    feat(guess-the-number): add tries and constants (#2)

--- guess-the-number/src/main.rs
+++ guess-the-number/src/main.rs
@@ -1,31 +1,38 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+const MAX_SECRET_NUMBER: u8 = 10;
+const AMOUNT_OF_TRIES: u8 = 3;
+
 fn main() {
     println!("Welcome to Guess the Number!");
     println!("I'm thinking of a number between 1 and 10.");
 
-    let secret_number = rand::thread_rng().gen_range(1..=10);
+    let secret_number = rand::thread_rng().gen_range(1..=MAX_SECRET_NUMBER);
 
-    loop {
+    for attempt in 1..=AMOUNT_OF_TRIES {
         let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line.");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: u8 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("That's too low, try again: "),
-            Ordering::Greater => println!("That's too high, try again:"),
             Ordering::Equal => {
                 println!("You got it! {guess} was the answer 👑");
                 break;
             }
+            Ordering::Less | Ordering::Greater if attempt == AMOUNT_OF_TRIES => {
+                println!("You ran out of tries!\n{secret_number} was the answer!");
+                break;
+            }
+            Ordering::Less => println!("That's too low, try again: "),
+            Ordering::Greater => println!("That's too high, try again:"),
         }
     }
 }
